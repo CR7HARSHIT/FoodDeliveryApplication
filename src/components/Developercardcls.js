e5fc8562c87bddbf9ad12c1f4f99cb8dccc73277
CR7HARSHIT <hr2697020@gmail.com>
2024-10-12 00:38:27 +0530

    Developer card created on About us

--- src/components/Developercardcls.js
+++ src/components/Developercardcls.js
@@ -34,17 +34,20 @@ class Developercardcls extends React.Component
 			avatar_url,
 			html_url
 	}=this.state.data;
+	console.log(`hiring value=${hireable}`)
 	return (
-	<div className="usercard">
+	<div className="developercard">
 			<h2>{"Developer"}</h2>
 			<img src={avatar_url}/>
-			<h3>Name:{name}</h3>
-			<h3>Location:{location}</h3>
-			<h3>Age:{21}</h3>
-			<h3>Bio:{bio}</h3>
-			<h3>Twitter_username:{twitter_username}</h3>
-			<h3>CurrentHiringStatus:{hireable}</h3>
-			<h3>Github_ID:<a href={html_url}/></h3>
+			<h3>Name:<span className="dc-span">{name}</span></h3>
+			<h3>Location:<span className="dc-span">{location}</span></h3>
+			<h3>Age:<span className="dc-span">{21}</span></h3>
+			<h3>Bio:<span className="dc-span">{bio}</span></h3>
+			<h3>CurrentHiringStatus:<span className="dc-span">{hireable=== true ? "Availabe For Hiring" : "Unknown" }</span></h3>
+			<h3>Twitter_ID:<span className="dc-span"><a href={"https://x.com/"+twitter_username}>{twitter_username}</a></span></h3>
+			<h3>Github_ID:<span className="dc-span"><a href={html_url}>{"CR7HARSHIT"}</a></span></h3>
+			<h3>Linkedin_ID:<span className="dc-span"><a href={"https://www.linkedin.com/in/harshvardhanrao15022004/"}>{"harshvardhanrao15022004"}</a></span></h3>
+			<h3>Email:<span  className="dc-span">  <a href={`mailto:${"hr2697020@gmail.com"}`}>{"hr2697020@gmail.com"}</a></span></h3>
 		</div>)
    }
 }
