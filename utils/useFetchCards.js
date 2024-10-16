b2da4d75061f93c8c81efc3cb4213a631504b5cd
CR7HARSHIT <hr2697020@gmail.com>
2024-10-16 20:44:36 +0530

    changes in proxyurl to bypass cors

--- utils/useFetchCards.js
+++ utils/useFetchCards.js
@@ -8,22 +8,33 @@ const useFetchCards=(status)=>{
 
    async function fetchdata() { 
 	const url=encodeURIComponent('https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.95250&lng=75.71050&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
-	const proxyUrl=`https://api.allorigins.win/get?url=${url}`;
+	const proxyUrl=`https://thingproxy.freeboard.io/fetch/${url}`;
   
 	let data;
 	try {
 	  console.log("fetch called");
 	  let response=await fetch(proxyUrl);
 	  console.log("fetch responded");
-	  let jsonData=await response.json();
+	  console.log(response)
+	  let x=await response.json();
 	  // Since the data is wrapped by the proxy, we need to parse the actual contents
-	  data=JSON.parse(jsonData.contents);
+	  data =  x;
 	} catch (error) {
 	  console.log(error);
 	}
-	console.log(data); 
-	console.log(data?.data?.cards[1]?.card?.card?.gridElements.infoWithStyle.restaurants)
-	setFetchCardsArray(data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle.restaurants); 
+	console.log(data);
+	// let arr= data.data.success.cards;
+	// for(let i=0;i<(arr.length);i++){
+	// 	const {restaurants}=data?.data.success.cards[i].gridWidget.gridElements.infoWithStyle
+	// 	if(typeof restaurants!== undefined)
+	// 	{
+	// 		setFetchCardsArray(restaurants);
+	// 		console.log(restaurants)
+	// 		break;
+	// 	}
+	// }
+	console.log(data.data.cards[2].card.card.gridElements.infoWithStyle.restaurants)
+	 setFetchCardsArray(data.data.cards[2].card.card.gridElements.infoWithStyle.restaurants)
 	
   }
    return FetchCardsArray;
--- utils/useFetchMenu.js
+++ utils/useFetchMenu.js
@@ -8,17 +8,18 @@ const useFetchMenu=(restid,status)=>{
 		
    
 	const url=encodeURIComponent(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.95250&lng=75.71050&restaurantId=${restid}&catalog_qa=undefined&submitAction=ENTER`);
-	const proxyUrl = `https://api.allorigins.win/get?url=${url}`;
+	const proxyUrl = `https://thingproxy.freeboard.io/fetch/${url}`;
   
 	let dataobj;
 	try {
 	  console.log("fetch called");
 	  let response=await fetch(proxyUrl);
 	  console.log("fetch responded");
-	  let jsonData=await response.json();
+	  console.log(response)
+	  let x=await response.json();
 	  // Since the data is wrapped by the proxy, we need to parse the actual contents
-	  
-	  dataobj=JSON.parse(jsonData.contents);
+	  console.log(x)
+	  dataobj = x
 	} catch (error) {
 	  console.log(error);
 	}
