8361bbfad10d3dfc9f96177c88c274dbba457d7a
CR7HARSHIT <hr2697020@gmail.com>
2024-10-31 16:17:18 +0530

    Cart feature done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,8 @@
 import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
-import { useState } from "react";
 const Cart=()=>{
 	const itemsarray=useSelector((store)=> store.cart.items);
-	const [value,setvalue]=useState(1);
-	const fn=(x)=> (setvalue(x))
+	
 	return(
 	  <div className="bg-gray-100 min-h-screen p-4">
          <h3 className="text-center font-extrabold text-3xl mb-8">CART ITEMS</h3>
@@ -13,7 +11,7 @@ const Cart=()=>{
             console.dir(itemobject);
              return (
                 <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4" key={itemobject.id}>
-                    <CartItem obj={itemobject} sv={value} svf={fn} />
+                    <CartItem obj={itemobject} itemarr={itemsarray}  />
                 </div>
              );
            })}
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,11 +1,11 @@
 import { useState } from "react"
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { changecount,deleteitem } from "../features/cartSlice";
-
-const CartItem=(props)=>{
-	const {itemdata,count}=props.obj
+const CartItem=({obj})=>{
+	const {itemdata,count}=obj
 	const [freq,setfreq]=useState(count);
 	const dispatch =useDispatch();
+	const itemarr=useSelector((state)=> state.cart.items)     
 	return(
 		<div className="flex justify-between items-center ">
 		<div className="w-9/12 m-4">
@@ -17,15 +17,26 @@ const CartItem=(props)=>{
 	   <div class="flex justify-between items-center">
        <span class="text-left ml-2 mr-2 mb-3 text-xl"  onClick={()=>{
 		if(freq-1===0) {
-			console.dir(props)
-			dispatch(deleteitem({id:itemdata.id}))
-			console.dir(props)
-			console.log(props.svf)
-			console.log(props.sv)
-			props.svf(props.sv+1)
+			let index=0;
+			console.dir(itemarr)
+			for(let i=0;i<itemarr.length;i++)
+			{
+                if(itemarr[i].itemdata.id===itemdata.id){
+					index=i;
+					break;
+				}
+			}
+			 console.log("before",itemarr)
+			 dispatch(deleteitem({id:itemdata.id}))
+			if(index!==itemarr.length-1){
+				console.log("After",itemarr)
+				 setfreq(itemarr[index+1].count)
+			}
 		}
-		else {dispatch(changecount({id:itemdata.id,sign:'-'}))
-		setfreq(freq-1)}
+		else {
+			dispatch(changecount({id:itemdata.id,sign:'-'}))
+		    setfreq(freq-1)
+		}
 	   }}>-</span>
        <span class="text-center  ml-2 mr-2 mb-3 text-xl">{freq}</span>
        <span class="text-right ml-2 mr-2 mb-3 text-xl"
