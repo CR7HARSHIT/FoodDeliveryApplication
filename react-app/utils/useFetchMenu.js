23f1b6be8221494fbc9cc80311c1622484465e2d
CR7HARSHIT <hr2697020@gmail.com>
2024-11-01 09:22:23 +0530

    created an server and configure it with existing application

---
+++ react-app/utils/useFetchMenu.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+const useFetchMenu=(restid,status)=>{
+  const [object,setobject]=useState(null)
+  useEffect(()=>{
+	if(status)
+	 {fetchdata();}
+  }
+  ,[])
+  async function fetchdata() { 
+		
+   
+	const url=encodeURIComponent(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.95250&lng=75.71050&restaurantId=${restid}&catalog_qa=undefined&submitAction=ENTER`);
+	 const proxyUrl = `http://localhost:3001/proxy?url=${url}`;
+  
+	let dataobj;
+	try {
+	  console.log("fetch called");
+	  let response=await fetch(proxyUrl);
+	  console.log("fetch responded");
+	  console.log(response)
+	  let x=await response.json();
+	  // Since the data is wrapped by the proxy, we need to parse the actual contents
+	  console.log(x)
+	  dataobj = x
+	} catch (error) {
+	  console.log(error);
+	}
+	
+	 setobject(dataobj)
+	 console.log(dataobj)
+  }
+  return object;
+}
+export default useFetchMenu;
+
