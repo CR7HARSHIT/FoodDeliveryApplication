4fdb8fbb6ffbd0b7596e9c9f7abc5e8703cda88c
CR7HARSHIT <hr2697020@gmail.com>
2025-01-29 14:12:50 +0530

    Bug fixed

--- client/utils/useFetchCards.js
+++ client/utils/useFetchCards.js
@@ -14,7 +14,7 @@ const useFetchCards=(status,location)=>{
 	},[location])
    
    async function fetchdata() { 
-	const url = encodeURIComponent(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=${latitude}&lng=${longitude}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`);
+	const url = encodeURIComponent(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.9124336&lng=75.7872709&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`);
     const proxyUrl = `${process.env.REACT_APP_BASE_URL}/proxy?url=${url}`;
   
     let data;
